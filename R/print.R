f5279f2b9624145c59606e0db6b170e3e8819a67
AQlT <alain.quartier@yahoo.fr>
2022-07-25 16:38:11 +0200

    plot

--- R/print.R
+++ R/print.R
@@ -62,3 +62,34 @@ print.JD3TRAMOSEATS_OUTPUT<- function(x, digits = max(3L, getOption("digits") - 
   print(x$result, digits = digits, ...)
 }
 
+#' @export
+plot.JD3_TRAMOSEATS_RSLTS <- function(x, first_date = NULL, last_date = NULL,
+                               type_chart = c("sa-trend", "seas-irr"),
+                               caption = c("sa-trend" = "Y, Sa, trend",
+                                           "seas-irr" = "Sea., irr.")[type_chart],
+                               colors = c(y = "#F0B400", t = "#1E6C0B", sa = "#155692",
+                                          s = "#1E6C0B", i = "#155692"),
+                               ...){
+  plot(rjd3sa::sa.decomposition(x),
+       first_date = first_date, last_date = last_date,
+       type_chart = type_chart,
+       caption = caption,
+       colors = colors,
+       ...)
+}
+#' @export
+plot.JD3TRAMOSEATS_OUTPUT <- function(x, first_date = NULL, last_date = NULL,
+                                type_chart = c("sa-trend", "seas-irr"),
+                                caption = c("sa-trend" = "Y, Sa, trend",
+                                            "seas-irr" = "Sea., irr.")[type_chart],
+                                colors = c(y = "#F0B400", t = "#1E6C0B", sa = "#155692",
+                                           s = "#1E6C0B", i = "#155692"),
+                                ...){
+  plot(x$result,
+       first_date = first_date, last_date = last_date,
+       type_chart = type_chart,
+       caption = caption,
+       colors = colors,
+       ...)
+}
+
